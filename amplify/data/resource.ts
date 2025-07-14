b858c01fd31f59903ca0bded49c145c000ab9fe0
vergarabcs-asurion <billcarlo.vergara@asurion.com>
2025-07-14 13:03:36 +0800

    Item model

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,11 +7,18 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
+  Item: a
     .model({
-      content: a.string(),
+      description: a.string(),
+      sell_price: a.float(),
+      quantity: a.float(),
+      low_stock_qty: a.float(),
+      img: a.string(),
+      qr: a.string(), // S3 key or URL
+      create_date: a.datetime(),
+      tags: a.string(), // comma-separated tags
     })
-    .authorization((allow) => [allow.publicApiKey()]),
+    .authorization((allow) => [allow.publicApiKey()])
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -31,7 +38,7 @@ Go to your frontend source code. From your client-side code, generate a
 Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
 WORK IN THE FRONTEND CODE FILE.)
 
-Using JavaScript or Next.js React Server Components, Middleware, Server 
+Using JavaScript or Next.js React Server Components, Middleware, Server
 Actions or Pages Router? Review how to generate Data clients for those use
 cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
 =========================================================================*/
