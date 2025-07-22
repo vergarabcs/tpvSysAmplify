9ce2e453e7b643b6678ad47e8beff0d24ec2f815
vergarabcs <vergarabcs@gmail.com>
2025-07-22 13:10:38 +0800

    clean up receive-stock

--- app/store/index.ts
+++ app/store/index.ts
@@ -1,4 +1,3 @@
 export { useNavigationStore } from './navigationStore';
 export { useItemsStore, type Item } from './itemsStore';
 export { useCartStore, type CartItem } from './cartStore';
-export { useStockStore, type StockItem } from './stockStore';
